53e8c9252c31ce7d74db53ee418a7cc636b9b346
vitor-dias1 <45134271+vitor-dias1@users.noreply.github.com>
2023-10-21 18:50:00 -0300

    Manipulando String's
    Fatiando as string's de acordo com o indice

---
+++ Fatiamento_String.py
@@ -0,0 +1,25 @@
+nome = "Vitor Dias dos Santos"
+
+#Retorna a letra dentro do indice
+print(nome[0])
+
+#Retorna a letra dentro do indice contando da ultima para a primeira
+print(nome[-2])
+
+#Retorna todas as letras até o indice
+print(nome[:9])
+
+#Retorna todas as letras depois do indice
+print(nome[10:])
+
+#Retorna todas as letras dentro da range do indice
+print(nome[10:15])
+
+#Retorna todas as letras dentro da range do indice pulando de duas em duas
+print(nome[10:20:2])
+
+#Retorna todas as letras da variavel
+print(nome[:])
+
+#Retorna todas as letras da variavel de forma invertida
+print(nome[:: -1])
